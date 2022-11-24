783c00a460f2b291baa66eac0534a12c1482c3ff
LeafField <109466326+LeafField@users.noreply.github.com>
2022-11-24 21:38:58 +0900

    記事の追加、ページネーション機能のさらなる追加

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { getAllPostIDs, getPostData, getNext } from "../../lib/post";
+import { getAllPostIDs, getPostData, getNext, getPrev } from "../../lib/post";
 
 export const getStaticPaths = async () => {
   const paths = getAllPostIDs();
@@ -14,21 +14,24 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id);
   const nextID = getNext(params.id);
+  const prevID = getPrev(params.id);
   return {
     props: {
       postData,
       nextID,
+      prevID,
     },
   };
 };
 
-export default function Posts({ postData, nextID }) {
+export default function Posts({ postData, nextID, prevID }) {
   return (
     <>
       <h1>{postData.title}</h1>
       <h2>{postData.date}</h2>
       <img src={`${postData.thumbnail}`} alt="" />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      <div>{prevID && <Link href={`/posts/${prevID}`}>前の記事へ</Link>}</div>
       <div>{nextID && <Link href={`/posts/${nextID}`}>次の記事へ</Link>}</div>
     </>
   );
